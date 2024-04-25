f67b7d59b323fd15304676805da34e5a0934c655
dhavalgondaliya27 <bhavnagondaliya77@gmail.com>
2024-04-25 16:41:55 +0530

    Second commit

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ app.use(express.static("public"))
 app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.get('/', (req, res) => {
+    res.send('Velocity is running');
+  });
 //routes import
 import contactRouter from "./routes/contact.router.js"
 //routes declaration
